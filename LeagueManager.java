9163b79106bb79ed4f0f7dcd9612ea53dc6a8b0c
Anny Patino <103137175+apatino16@users.noreply.github.com>
2024-11-13 19:17:20 -0800

    Refactor code
    Worked on the LeagueManager.java logic and debugged code

--- LeagueManager.java
+++ LeagueManager.java
@@ -8,19 +8,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LeagueManager {
-    private Team mTeam;
-    private BufferedReader mReader;
+    private ArrayList<Team> mTeams = new ArrayList<>();
+    private BufferedReader mReader = new BufferedReader(new InputStreamReader(System.in));
     private Map<String, String> mMenu;
 
-
     // Method that displays menu options to the user
-    public void LeagueManager(Team team) {
-        mTeam = team;
-        mReader = new BufferedReader(new InputStreamReader(System.in));
-        mMenu = new HashMap<String, String>();
+    public LeagueManager() {
+        mMenu = new HashMap<>();
         mMenu.put("create", "Create a new team");
+        mMenu.put("quit", "Quit the program");
     }
 
     private Team promptTeamCreation() throws IOException {
@@ -31,39 +31,46 @@ public class LeagueManager {
         return new Team(teamName, coachName);
     }
 
-
-    public static void main(String[] args) {
-        Player[] players = Players.load();
-        System.out.printf("There are currently %d registered players.%n", players.length);
-
-        public void promptAction () {
-            // TODO:
+    public void printInstructions() {
+        System.out.println("\n Menu Options:");
+        for (Map.Entry<String, String> option : mMenu.entrySet()) {
+            System.out.printf("%s - %s%n", option.getKey(), option.getValue());
         }
+    }
 
-        // The  method provides a menu item that allows the Organizer to create a new team for the season
-        public void printInstructions () {
-            String command = "";
-
-            do {
-                try {
-                    command = // promptAction();
-
-                            switch (command) {
-                                case "create":
-                                    Team team = promptTeamCreation();
-                                    break;
-                                case "quit":
-                                    System.out.println("Terminated the League manager program.");
-                                    break;
-                            } catch(IOException ioe){
-                        System.out.println("Problem with input");
-                        ioe.printStackTrace();
-                    }
-                    while (!command.equals("quit")) ;
+    // The  method provides a menu item that allows the Organizer to create a new team for the season
+    public void runMenu() {
+        String command = "";
+        printInstructions();
+        try {
+            while (!command.equals("quit")) {
+                System.out.print("What would you like to do: ");
+                command = mReader.readLine();
+                switch (command) {
+                    case "create":
+                        Team team = promptTeamCreation();
+                        break;
+                    case "quit":
+                        System.out.println("Exiting the League manager program...");
+                        break;
+                    default:
+                        System.out.println("Unknown command, please try again.");
+                        printInstructions();
                 }
             }
-
+        } catch (IOException ioe) {
+            System.out.println("Problem with input");
+            ioe.printStackTrace();
         }
+
+    }
+
+    public static void main(String[] args) {
+
+        Player[] players = Players.load();
+        System.out.printf("There are currently %d registered players.%n", players.length);
+        new LeagueManager().runMenu();
+
     }
 
 }
